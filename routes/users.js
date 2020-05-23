e5e20e5a8e03f812c7d1ce3f3a27bf07842047dd
machado-jose <machado.josegustavo@gmail.com>
2020-05-23 11:29:06 -0300

    Configuring the DB and loading the menu

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
+const conn = require('./../inc/db');
+
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  
+	// simple query
+	conn.query(
+	  'SELECT * FROM tb_users ORDER BY name',
+	  function(err, results, fields) {
+	    (err) ? res.send(err) : res.send(results);
+	  }
+	);
+
 });
 
 module.exports = router;
